589e9dc085d0665bb1b12625f06fd283f94790da
enwask <enwask@ucf.edu>
2024-10-05 19:24:18 -0400

    Implement RMP data prefetching

--- understudy/api/mongo/client.py
+++ understudy/api/mongo/client.py
@@ -5,15 +5,13 @@ __all__ = ['db']
 
 
 class db:
+    """
+    MongoDB database connections.
+    """
+
     professors = st.connection(
         'mongodb',
         collection='professors',
-        type=MongoDBConnection,
-    )
-
-    professors_by_course = st.connection(
-        'mongodb',
-        collection='professors_by_course',
         type=MongoDBConnection,
     )
 
---
+++ understudy/prefetch.py
@@ -0,0 +1,60 @@
+import asyncio
+
+from understudy.api.mongo.client import db
+from understudy.api.rmp.data import get_professors, get_ratings
+from understudy.api.rmp.rating import Rating
+
+
+async def main():
+    """
+    Prefetches RMP data and loads it into the Mongo database.
+    """
+
+    # Fetch all professors
+    professors = await get_professors()
+
+    # Drop stale data and write professors to the Mongo database
+    db.professors.delete({})
+    db.professors.insert([
+        prof.to_dict()
+        for prof in professors.values()
+    ])
+
+    # Map of professor -> { course -> [Rating...] }
+    prof_ratings: dict[str, dict[str, list[Rating]]] = {}
+
+    # Fetch ratings for professors asynchronously
+    # We do this in batches to avoid running out of memory
+    batch_size = 10
+    for start in range(0, len(professors), batch_size):
+        tasks = []
+        for id, prof in list(professors.items())[start:start + batch_size]:
+            tasks.append(asyncio.create_task(get_ratings(id, prof.courses)))
+        res = await asyncio.gather(*tasks)
+        for id, ratings_by_course in res:
+            prof_ratings[id] = ratings_by_course
+
+        # Artificial throttling to avoid DNS issues
+        await asyncio.sleep(0.1)
+
+    # Drop old ratings data
+    db.ratings.delete({})
+
+    # Write ratings to the Mongo database
+    for prof, ratings_by_course in prof_ratings.items():
+        for course, ratings in ratings_by_course.items():
+            if len(ratings) == 0:
+                # Empty docs list fails for some reason
+                continue
+
+            db.ratings.insert([
+                {
+                    'professor': prof,
+                    'course': course,
+                    'rating': r.to_dict()
+                }
+                for r in ratings
+            ])
+
+if __name__ == "__main__":
+    asyncio.run(main())
